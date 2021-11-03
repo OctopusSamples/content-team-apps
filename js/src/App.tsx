604440b776de1db9c28d97875af6aec96878b164
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-03 10:57:37 +1000

    Made editor format a configurable variable

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -17,7 +17,7 @@ import {DynamicConfig} from "./config/dynamicConfig";
 
 // define app context
 export const AppContext = React.createContext<DynamicConfig>({
-    settings: {basename: "", title: "", generateApiPath: ""},
+    settings: {basename: "", title: "", generateApiPath: "", editorFormat: ""},
     useDefaultTheme: true
 });
 
