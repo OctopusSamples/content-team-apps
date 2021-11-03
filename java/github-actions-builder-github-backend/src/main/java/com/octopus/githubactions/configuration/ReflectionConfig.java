0644291909a3e403f6aa2762a6f4fcc4dbe36933
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-03 10:27:25 +1000

    Fixed import

--- java/github-actions-builder-github-backend/src/main/java/com/octopus/githubactions/configuration/ReflectionConfig.java
+++ java/github-actions-builder-github-backend/src/main/java/com/octopus/githubactions/configuration/ReflectionConfig.java
@@ -1,12 +1,20 @@
 package com.octopus.githubactions.configuration;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import com.octopus.githubactions.builders.dsl.*;
 
 /**
  * An empty class used to list classes that we need to expose for reflection.
  */
-@RegisterForReflection(targets = {Build.class, Jobs.class, On.class, Push.class, RunStep.class,
-    UsesWith.class, Workflow.class, WorkflowDispatch.class})
+@RegisterForReflection(targets = {
+    Build.class,
+    Jobs.class,
+    On.class,
+    Push.class,
+    RunStep.class,
+    UsesWith.class,
+    Workflow.class,
+    WorkflowDispatch.class})
 public class ReflectionConfig {
 
 }
