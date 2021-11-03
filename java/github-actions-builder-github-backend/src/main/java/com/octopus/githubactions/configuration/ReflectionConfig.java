4353933b72830d6fcf0a34e62eee20228d5c4e80
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-03 10:26:10 +1000

    Fixed checkstyle issues

--- java/github-actions-builder-github-backend/src/main/java/com/octopus/githubactions/configuration/ReflectionConfig.java
+++ java/github-actions-builder-github-backend/src/main/java/com/octopus/githubactions/configuration/ReflectionConfig.java
@@ -1,9 +1,12 @@
 package com.octopus.githubactions.configuration;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import com.octopus.githubactions.builders.dsl.*;
 
-@RegisterForReflection(targets = {Build.class, Jobs.class, On.class, Push.class, RunStep.class, UsesWith.class, Workflow.class, WorkflowDispatch.class})
+/**
+ * An empty class used to list classes that we need to expose for reflection.
+ */
+@RegisterForReflection(targets = {Build.class, Jobs.class, On.class, Push.class, RunStep.class,
+    UsesWith.class, Workflow.class, WorkflowDispatch.class})
 public class ReflectionConfig {
 
 }
