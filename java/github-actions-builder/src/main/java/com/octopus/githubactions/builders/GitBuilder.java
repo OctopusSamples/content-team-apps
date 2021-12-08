f565f6a20a13a3c2fe30302d6d3df2d414f92f90
Matthew Casperson <matthewcasperson@gmail.com>
2021-12-09 07:54:56 +1000

    Embedded attempt into release name

--- java/github-actions-builder/src/main/java/com/octopus/githubactions/builders/GitBuilder.java
+++ java/github-actions-builder/src/main/java/com/octopus/githubactions/builders/GitBuilder.java
@@ -83,8 +83,8 @@ public class GitBuilder {
                 .build())
         .with(
             new ImmutableMap.Builder<String, String>()
-                .put("tag_name", "${{ steps.determine_version.outputs.semVer }}.${{ github.run_number }}")
-                .put("release_name", "Release ${{ steps.determine_version.outputs.semVer }}.${{ github.run_number }}")
+                .put("tag_name", "${{ steps.determine_version.outputs.semVer }}-run${{ github.run_number }}attempt${{ github.run_attempt }}")
+                .put("release_name", "Release ${{ steps.determine_version.outputs.semVer }}-run${{ github.run_number }}attempt${{ github.run_attempt }}")
                 .put("draft", "false")
                 .put("prerelease", "false")
                 .build())
