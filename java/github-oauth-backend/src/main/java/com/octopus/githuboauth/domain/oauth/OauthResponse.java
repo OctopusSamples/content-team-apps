5ad96c4e7f5ddc697b2f63ce1608103c5ff35ee7
Matthew Casperson <matthewcasperson@gmail.com>
2022-01-24 15:34:11 +1000

    Fixed the response object

--- java/github-oauth-backend/src/main/java/com/octopus/githuboauth/domain/oauth/OauthResponse.java
+++ java/github-oauth-backend/src/main/java/com/octopus/githuboauth/domain/oauth/OauthResponse.java
@@ -5,17 +5,12 @@ import lombok.Data;
 
 /**
  * Represents the response from the GitHub OAuth service when exchanging a code for a token.
+ * https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#response
  */
 @Data
 public class OauthResponse {
   @JsonProperty("access_token")
   private String accessToken;
-  @JsonProperty("expires_in")
-  private int expiresIn;
-  @JsonProperty("refresh_token")
-  private String refreshToken;
-  @JsonProperty("refresh_token_expires_in")
-  private int refreshTokenExpiresIn;
   private String scope;
   @JsonProperty("token_type")
   private String tokenType;
