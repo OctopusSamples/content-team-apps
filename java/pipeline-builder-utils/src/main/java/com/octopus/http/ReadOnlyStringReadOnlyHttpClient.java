d071735a20e8380767c559efbba68f84419ef230
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-07 12:15:06 +1000

    Ensure the headers are sorted so the key is consistent everytime

--- java/pipeline-builder-utils/src/main/java/com/octopus/http/ReadOnlyStringReadOnlyHttpClient.java
+++ java/pipeline-builder-utils/src/main/java/com/octopus/http/ReadOnlyStringReadOnlyHttpClient.java
@@ -28,7 +28,8 @@ import org.jboss.logging.Logger;
  * A readonly HTTP client. This is important, because GitHub does not offer a readonly scope when
  * accessing repos: https://github.com/github/feedback/discussions/7891. So by ensuring we use a
  * client that can only make GET or HEAD calls, we can be sure we don't make any unwanted
- * modifications. All GET and HEAD requests are cached to help prevent GitHub API rate limit issues.
+ * modifications. All GET and HEAD requests are cached to help prevent GitHub API rate limit
+ * issues.
  */
 public class ReadOnlyStringReadOnlyHttpClient implements ReadOnlyHttpClient {
 
@@ -265,7 +266,10 @@ public class ReadOnlyStringReadOnlyHttpClient implements ReadOnlyHttpClient {
     final StringBuilder sb = new StringBuilder(method + "\n" + url);
     if (headers != null) {
       sb.append("\n");
-      sb.append(headers.stream().map(h -> h.getName() + ":" + h.getValue())
+      sb.append(headers
+          .stream()
+          .map(h -> h.getName() + ":" + h.getValue())
+          .sorted()
           .collect(Collectors.joining("\n")));
     }
     return sb.toString();
