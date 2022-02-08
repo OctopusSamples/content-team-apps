5bb3e7f5f4b57042c397e9ad69d129714e2f5ba9
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-09 07:16:55 +1000

    Fixed lint error

--- js/src/pages/GitHubLogin.tsx
+++ js/src/pages/GitHubLogin.tsx
@@ -40,7 +40,7 @@ const GitHubLogin: FC<{}> = (): ReactElement => {
                     <h2>You must log into GitHub to continue.</h2>
                     <h4>Logging into GitHub provides access to private repositories and increases rate limits.</h4>
                     <h4>By logging in, you agree that your GitHub email address may be used for marketing purposes.</h4>
-                    <h4>View the <a target={"_blank"} href={"https://octopus.com/legal/privacy"}>Octopus privacy policy</a> for more information.</h4>
+                    <h4>View the <a target={"_blank"} rel={"noreferrer"} href={"https://octopus.com/legal/privacy"}>Octopus privacy policy</a> for more information.</h4>
                 </Grid>
                 <Grid item md={2} sm={false} xs={false}/>
                 <Grid item md={2} sm={false} xs={false}/>
