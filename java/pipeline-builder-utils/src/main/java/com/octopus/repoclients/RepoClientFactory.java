4c5c70507274238141ff19f11b6cdab27a467b5e
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-08 10:27:08 +1000

    Made comments a little clearer

--- java/pipeline-builder-utils/src/main/java/com/octopus/repoclients/RepoClientFactory.java
+++ java/pipeline-builder-utils/src/main/java/com/octopus/repoclients/RepoClientFactory.java
@@ -2,8 +2,8 @@ package com.octopus.repoclients;
 
 /**
  * Repo clients usually share a base set of credentials, defined once in an implementation of this
- * factory. Then each request will have it's own unique repo and access token, so a new client is
- * created with each request.
+ * factory. Then each request will have its own unique repo and access token, so a new client is
+ * created with each request, sharing the same base credentials.
  */
 public interface RepoClientFactory {
 
