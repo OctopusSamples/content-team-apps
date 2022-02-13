3d2c703003742d825e045cc31cfc1b36e9bbe894
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-14 08:23:28 +1000

    Fixed tests

--- java/audit-service/src/test/java/com/octopus/audits/PartitionIdentifierTest.java
+++ java/audit-service/src/test/java/com/octopus/audits/PartitionIdentifierTest.java
@@ -1,13 +1,17 @@
 package com.octopus.audits;
 
 import com.octopus.audits.domain.utilities.PartitionIdentifier;
+import io.quarkus.test.junit.QuarkusTest;
 import java.util.ArrayList;
+import javax.inject.Inject;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.locationtech.jts.util.Assert;
 
+@QuarkusTest
 public class PartitionIdentifierTest {
-  public static final PartitionIdentifier PARTITION_IDENTIFIER = new PartitionIdentifier();
+  @Inject
+  PartitionIdentifier partitionIdentifier;
 
   @ParameterizedTest
   @CsvSource({
@@ -17,39 +21,16 @@ public class PartitionIdentifierTest {
     "testing,testing",
     "testing,testing ",
     "testing, testing ",
-    "main,",
-    "main, ",
-    "main,application/vnd.api+json; ",
-    "main,application/vnd.api+json",
     "main, ",
     "main,"
   })
-  public void testPartitions(final String expected, final String acceptHeader) {
+  public void testPartitions(final String expected, final String dataPartitionHeader) {
     Assert.equals(
         expected,
-        PARTITION_IDENTIFIER.getPartition(
+        partitionIdentifier.getPartition(
             new ArrayList<>() {
               {
-                add(acceptHeader);
-              }
-            },
-            ""));
-    Assert.equals(
-        expected,
-        PARTITION_IDENTIFIER.getPartition(
-            new ArrayList<>() {
-              {
-                add("application/vnd.api+json," + acceptHeader);
-              }
-            },
-            ""));
-    Assert.equals(
-        expected,
-        PARTITION_IDENTIFIER.getPartition(
-            new ArrayList<>() {
-              {
-                add(acceptHeader);
-                add("application/vnd.api+json");
+                add(dataPartitionHeader);
               }
             },
             ""));
