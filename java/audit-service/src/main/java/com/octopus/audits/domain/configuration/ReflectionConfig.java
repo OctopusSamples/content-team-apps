952ca5af58309dd2916480e0d5e230d68908a46c
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-15 07:57:59 +1000

    Wait a minute - the error is for "Links", not "Link" >:(

--- java/audit-service/src/main/java/com/octopus/audits/domain/configuration/ReflectionConfig.java
+++ java/audit-service/src/main/java/com/octopus/audits/domain/configuration/ReflectionConfig.java
@@ -1,15 +1,15 @@
 package com.octopus.audits.domain.configuration;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.deser.std.DateDeserializers.TimestampDeserializer;
 import com.github.jasminb.jsonapi.Link;
+import com.github.jasminb.jsonapi.Links;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 /**
  * An empty class used to list classes that we need to expose for reflection.
  */
 @RegisterForReflection(
-    targets = {TimestampDeserializer.class, Link.class},
+    targets = {TimestampDeserializer.class, Link.class, Links.class},
     ignoreNested = false)
 public class ReflectionConfig {
 
--- java/audit-service/src/main/java/com/octopus/audits/domain/framework/producers/JsonApiConverter.java
+++ java/audit-service/src/main/java/com/octopus/audits/domain/framework/producers/JsonApiConverter.java
@@ -1,14 +1,10 @@
 package com.octopus.audits.domain.framework.producers;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.github.jasminb.jsonapi.DeserializationFeature;
 import com.github.jasminb.jsonapi.Link;
 import com.github.jasminb.jsonapi.ResourceConverter;
 import com.octopus.audits.domain.entities.Audit;
 import com.octopus.audits.domain.entities.Health;
-import com.octopus.audits.domain.framework.producers.JsonApiConverter.LinkAccessor.LinkAccessorSerializer;
 import javax.enterprise.inject.Produces;
 
 /** Produces a JSONAPI resource converter. */
@@ -21,22 +17,9 @@ public class JsonApiConverter {
    */
   @Produces
   public ResourceConverter buildResourceConverter() {
-    final ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-    final SimpleModule module = new SimpleModule();
-    module.addSerializer(Link.class, new LinkAccessorSerializer());
-    objectMapper.registerModule(module);
-
-    final ResourceConverter resourceConverter = new ResourceConverter(objectMapper, Audit.class, Health.class);
+    final ResourceConverter resourceConverter = new ResourceConverter(Audit.class, Health.class);
     resourceConverter.disableDeserializationOption(DeserializationFeature.REQUIRE_RESOURCE_ID);
     resourceConverter.enableDeserializationOption(DeserializationFeature.ALLOW_UNKNOWN_INCLUSIONS);
     return resourceConverter;
-  }
-
-  protected static class LinkAccessor extends Link {
-    protected static class LinkAccessorSerializer extends Link.LinkSerializer {
-
-    }
   }
 }
