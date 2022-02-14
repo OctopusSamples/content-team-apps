4c5407f31cb7e5e987fdd06368c32a5eed764281
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-14 19:47:51 +1000

    Removed duplicate class

--- java/audit-service/src/main/java/com/octopus/audits/domain/framework/producers/JsonApiConverter.java
+++ java/audit-service/src/main/java/com/octopus/audits/domain/framework/producers/JsonApiConverter.java
@@ -18,7 +18,7 @@ public class JsonApiConverter {
   @Produces
   public ResourceConverter buildResourceConverter() {
     final ResourceConverter resourceConverter =
-        new ResourceConverter(Audit.class, Audit.class, Health.class, Link.class);
+        new ResourceConverter(Audit.class, Health.class, Link.class);
     resourceConverter.disableDeserializationOption(DeserializationFeature.REQUIRE_RESOURCE_ID);
     resourceConverter.enableDeserializationOption(DeserializationFeature.ALLOW_UNKNOWN_INCLUSIONS);
     return resourceConverter;
