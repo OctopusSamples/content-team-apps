3a843a33d73faefbf9e4e5c21c2212adf12cd20b
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-14 13:04:22 +1000

    Adding server side paging

--- js/src/pages/Audits.tsx
+++ js/src/pages/Audits.tsx
@@ -33,35 +33,41 @@ const useStyles = makeStyles(() =>
     })
 );
 
+const ROWS_PER_PAGE = 5;
+
 const Audits: FC<{}> = (): ReactElement => {
 
     const context = useContext(AppContext);
     const classes = useStyles();
 
     const [audits, setAudits] = useState<AuditsCollection | null>(null);
+    const [page, setPage] = useState<number>(0);
     const [error, setError] = useState<string | null>(null);
 
     const columns = [
         {field: 'id', headerName: 'Id', width: 70},
-        {field: 'time', headerName: 'TimeStamp', width: 70},
-        {field: 'subject', headerName: 'Subject', width: 200},
+        {field: 'time', headerName: 'Time Stamp', width: 200},
+        {field: 'subject', headerName: 'Subject', width: 300},
         {field: 'action', headerName: 'Action', width: 200},
         {field: 'object', headerName: 'Object', width: 200},
         {field: 'dataPartition', headerName: 'Data Partition', width: 200},
     ];
 
     useEffect(() => {
-        getJsonApi<AuditsCollection>(context.settings.auditEndpoint, context.partition)
+        getJsonApi<AuditsCollection>(context.settings.auditEndpoint + "?page[limit]=" + ROWS_PER_PAGE + "&page[offset]=0", context.partition)
             .then(data => setAudits(data))
             .catch(() => setError("Failed to retrieve audit resources. "
                 + (isBranchingEnabled() ? "Branching rules are enabled - double check they are valid, or disable them." : "")))
     }, [setAudits, context.settings.auditEndpoint, context.partition]);
 
-    const refresh = () => {
+    const refresh = (page:number) => {
         setAudits(null);
         setError(null);
-        getJsonApi<AuditsCollection>(context.settings.auditEndpoint, context.partition)
-            .then(data => setAudits(data))
+        getJsonApi<AuditsCollection>(context.settings.auditEndpoint + "?page[offset]=" + (page * ROWS_PER_PAGE) + "&page[limit]=" + ROWS_PER_PAGE, context.partition)
+            .then(data => {
+                setAudits(data);
+                setPage(page);
+            })
             .catch(() => setError("Failed to retrieve audit resources. "
                 + (isBranchingEnabled() ? "Branching rules are enabled - double check they are valid, or disable them." : "")))
     }
@@ -79,12 +85,15 @@ const Audits: FC<{}> = (): ReactElement => {
                     <div>{error}</div>
                 </Grid>
                 <Grid xs={12}>
-                    <Button variant={"outlined"} onClick={refresh}>Refresh</Button>
+                    <Button variant={"outlined"} onClick={() => refresh(0)}>Reload</Button>
                 </Grid>
             </Grid>}
             {audits && <Grid container={true}>
                 <Grid xs={12} className={classes.mainContent}>
                     <DataGrid
+                        pagination
+                        paginationMode="server"
+                        rowCount={1000}
                         rows={(audits.data || []).map((a: Audit) => ({
                             id: a.id,
                             time: a.attributes.time,
@@ -93,13 +102,15 @@ const Audits: FC<{}> = (): ReactElement => {
                             object: a.attributes.object,
                             dataPartition: a.attributes.dataPartition
                         }))}
+                        page={page}
                         columns={columns}
-                        pageSize={5}
-                        rowsPerPageOptions={[5]}
+                        pageSize={ROWS_PER_PAGE}
+                        rowsPerPageOptions={[ROWS_PER_PAGE]}
+                        onPageChange={(page) => refresh((page))}
                     />
                 </Grid>
                 <Grid xs={12} className={classes.buttonRow}>
-                    <Button variant={"outlined"} onClick={refresh}>Refresh</Button>
+                    <Button variant={"outlined"} onClick={() => refresh(0)}>Reload</Button>
                 </Grid>
             </Grid>}
         </>
