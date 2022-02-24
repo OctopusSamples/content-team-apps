77aabec836d53073a58c2b6963d2fc31ce5bd6cd
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-24 15:20:31 +1000

    Started filling out the initial options

--- js/app-generator/src/components/TargetSelection.tsx
+++ js/app-generator/src/components/TargetSelection.tsx
@@ -1,9 +1,8 @@
 import {FC, ReactElement, useContext} from "react";
 import {Helmet} from "react-helmet";
-import { makeStyles } from '@mui/styles';
-import {Grid} from "@mui/material";
+import {makeStyles} from '@mui/styles';
+import {Button, Grid} from "@mui/material";
 import {AppContext} from "../App";
-import {Button} from "@mui/material";
 import {SxProps} from "@mui/system";
 import {Theme} from "@mui/material/styles";
 
@@ -16,13 +15,22 @@ const useStyles = makeStyles((theme) => {
             minWidth: "100%",
             minHeight: "100%",
             justifyContent: "center"
+        },
+        column: {
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            minWidth: "100%",
+            minHeight: "100%",
+            justifyContent: "top"
         }
     }
 });
 
-const buttonStyle:SxProps<Theme> = {
+const buttonStyle: SxProps<Theme> = {
     width: '100%',
     minHeight: '64px',
+    marginTop: '8px'
 }
 
 const TargetSelection: FC = (): ReactElement => {
@@ -41,13 +49,32 @@ const TargetSelection: FC = (): ReactElement => {
                 className={classes.root}
                 spacing={2}
             >
-                <Grid item md={4} xs={false}/>
-                <Grid item md={4} xs={12}>
-                    <Button sx={buttonStyle} variant="outlined">
-                        {"EKS with GitHub and Octopus"}
-                    </Button>
+                <Grid item md={3} xs={0}/>
+                <Grid item md={6} xs={12}>
+                    <Grid
+                        container={true}
+                        className={classes.column}
+                    >
+                        <h2>Where are you deploying the microservice?</h2>
+                        <p>
+                            Select the platform that you wish to deploy the sample microservice application to in
+                            Octopus. If you only want to download the application code, select the last option.
+                        </p>
+                        <Button sx={buttonStyle} variant="outlined">
+                            {"AWS Elastic Kubernetes Engine (EKS)"}
+                        </Button>
+                        <Button sx={buttonStyle} variant="outlined">
+                            {"AWS Elastic Compute Service (ECS)"}
+                        </Button>
+                        <Button sx={buttonStyle} variant="outlined">
+                            {"AWS Lambda"}
+                        </Button>
+                        <Button sx={buttonStyle} variant="outlined">
+                            {"Download the code with no CI/CD configuration"}
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item md={4} xs={false}/>
+                <Grid item md={3} xs={0}/>
             </Grid>
         </>
     );
