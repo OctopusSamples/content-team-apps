bb68b0ce33a3c5bea4975da467d61c3c04009d4c
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-28 09:55:38 +1000

    Generate and validate nonce

--- js/app-generator/src/components/journey/LogIntoOctopus.tsx
+++ js/app-generator/src/components/journey/LogIntoOctopus.tsx
@@ -2,16 +2,27 @@ import {FC, ReactElement} from "react";
 import {Button, Grid, Link} from "@mui/material";
 import {journeyContainer, nextButtonStyle} from "../../utils/styles";
 import {JourneyProps} from "../../statemachine/appBuilder";
+import Cookies from "js-cookie";
+
+// define the randomUUID function
+declare global {
+    interface Crypto {
+        randomUUID: () => string;
+    }
+}
 
 const LogIntoOctopus: FC<JourneyProps> = (props): ReactElement => {
     const classes = journeyContainer();
 
     const login = () => {
+        const nonce = crypto.randomUUID().toString().replaceAll("-", "").substr(0, 19);
+        Cookies.set("appBuilderOctopusNonce", nonce);
         localStorage.setItem("appBuilderState", "loggedIntoOctopus");
         window.open("https://octopus.com/oauth2/authorize?"
             + "client_id=855b8e5a-c3c4-4c4d-91b1-fef5dd762ec2&scope=openid%20profile%20email"
             + "&response_type=code+id_token"
-            + "&response_mode=form_post&nonce=dhIdjCXzcBPsaYUJuUQ"
+            + "&response_mode=form_post"
+            + "&nonce=" + nonce
             + "&redirect_uri=http://localhost:10000/oauth/octopus",
             "_parent");
     }
