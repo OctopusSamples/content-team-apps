2c9ffb11102dfd3c05db26dd6ed6767d8ed18ac3
Matthew Casperson <matthewcasperson@gmail.com>
2022-02-28 14:31:35 +1000

    First test generator works!

--- js/generator-github-complete-eks-deployment/generators/app/index.js
+++ js/generator-github-complete-eks-deployment/generators/app/index.js
@@ -1,14 +1,32 @@
+const crypto = require('crypto');
 const Generator = require('yeoman-generator');
+
 module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+
+        this.s3_bucket_suffix = crypto.randomUUID();
+        this.option("aws_region", {type: "string"});
+    }
+
     initializing() {
-        this.composeWith(require.resolve('generator-github-shared-space/generators/app'));
+        this.composeWith(
+            require.resolve('generator-github-shared-space/generators/app'),
+            {
+                s3_bucket_suffix: this.s3_bucket_suffix,
+                aws_region: this.options.aws_region
+            });
     }
 
     writing() {
         this.fs.copyTpl(
             this.templatePath('.github/workflows/eks-deployment.yaml'),
             this.destinationPath('.github/workflows/eks-deployment.yaml'),
-            { }
+            {
+                octopus_project: "Octo App Builder",
+                s3_bucket_suffix: this.s3_bucket_suffix,
+                aws_region: this.options.aws_region
+            }
         );
     }
 };
--- js/generator-github-shared-space/generators/app/index.js
+++ js/generator-github-shared-space/generators/app/index.js
@@ -1,16 +1,29 @@
 const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+
+        this.option("s3_bucket_suffix");
+        this.option("aws_region");
+    }
+
     writing() {
         this.fs.copyTpl(
-            this.templatePath('github/action.yaml'),
-            this.destinationPath('github/action.yaml'),
-            { }
+            this.templatePath('github/shared-space/action.yaml'),
+            this.destinationPath('github/shared-space/action.yaml'),
+            {
+                s3_bucket_suffix: this.options.s3_bucket_suffix,
+                aws_region: this.options.aws_region,
+            }
         );
 
         this.fs.copyTpl(
             this.templatePath('octopus/*.tf'),
             this.destinationPath('octopus/*.tf'),
-            { s3_bucket_suffix: crypto.randomUUID()}
+            {
+                s3_bucket_suffix: this.options.s3_bucket_suffix,
+                aws_region: this.options.aws_region
+            }
         );
     }
 };
