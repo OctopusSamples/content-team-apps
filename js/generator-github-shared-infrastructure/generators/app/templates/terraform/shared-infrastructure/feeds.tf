4bc708a00b11485c159951717d043b058af40839
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-02 14:15:28 +1000

    Started linking up all the variables

--- js/generator-github-shared-infrastructure/generators/app/templates/terraform/shared-infrastructure/feeds.tf
+++ js/generator-github-shared-infrastructure/generators/app/templates/terraform/shared-infrastructure/feeds.tf
@@ -3,11 +3,19 @@ resource "octopusdeploy_docker_container_registry" "dockerhub_feed" {
   name          = "DockerHub"
 }
 
+output "octopus_github_docker_feed_id" {
+  value = octopusdeploy_docker_container_registry.dockerhub_feed.id
+}
+
 resource "octopusdeploy_docker_container_registry" "github_docker_feed" {
   feed_uri = "https://gchr.io"
   name     = "GitHub Docker"
   password = var.github_feed_token
   username = var.github_username
+}
+
+output "octopus_dockerhub_feed_id" {
+  value = octopusdeploy_docker_container_registry.github_docker_feed.id
 }
 
 resource "octopusdeploy_maven_feed" "github_maven_feed" {
@@ -17,4 +25,8 @@ resource "octopusdeploy_maven_feed" "github_maven_feed" {
   password                       = var.github_feed_token
   name                           = "GitHub Maven"
   username                       = var.github_username
+}
+
+output "octopus_github_maven_feed_id" {
+  value = octopusdeploy_maven_feed.github_maven_feed.id
 }
