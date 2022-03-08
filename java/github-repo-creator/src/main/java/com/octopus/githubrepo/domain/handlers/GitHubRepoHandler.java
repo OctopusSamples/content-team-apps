680f2ef80db213889e8cf3cf40490af8bee1be33
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-08 11:14:18 +1000

    Fixed typo

--- java/github-repo-creator/src/main/java/com/octopus/githubrepo/domain/handlers/GitHubRepoHandler.java
+++ java/github-repo-creator/src/main/java/com/octopus/githubrepo/domain/handlers/GitHubRepoHandler.java
@@ -239,14 +239,13 @@ public class GitHubRepoHandler {
                 .content(Base64.getEncoder().encodeToString(
                     """
                       # App Builder
-                      This repo was populated by the Octopus App Builder tool. The directory structure
-                      is shown below:
+                      This repo was populated by the Octopus App Builder tool. The directory structure is shown below:
                       
                       * `.github/workflows`: GitHub Action Workflows that populate a cloud Octopus instance, build and deploy the sample code, and initiate a deployment in Octopus.
                       * `github`: Composable GitHub Actions that are called by the workflow files.
                       * `terraform`: Terraform templates used to create cloud resources and populate the Octopus cloud instance.
                       * `java`: The sample Java application.
-                      * `ja`: The sample JavaScript application.
+                      * `js`: The sample JavaScript application.
                       * `dotnet`: The sample DotNET application.
                     """.getBytes(
                     StandardCharsets.UTF_8)))
