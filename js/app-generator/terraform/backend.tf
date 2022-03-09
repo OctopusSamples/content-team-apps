5bd878b9c252285fad4af8edb28ea57e5d5d3d6c
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-10 07:06:39 +1000

    Fixing up the terraform backend

--- js/app-generator/terraform/backend.tf
+++ js/app-generator/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "app-builder-<%= s3_bucket_suffix %>"
-    key    = "appbuilder-frontend"
-    region = "<%= aws_state_bucket_region %>"
+    bucket = "app-builder-c81b45ae-50de-466c-8500-3845fd8b80c"
+    key    = "app-builder-frontend"
+    region = "us-west-1"
   }
 }
--- js/app-generator/terraform/required_providers.tf
+++ js/app-generator/terraform/required_providers.tf
@@ -9,10 +9,4 @@ terraform {
       version = "4.3.0"
     }
   }
-
-  backend "s3" {
-    bucket = "app-builder-c81b45ae-50de-466c-8500-3845fd8b80c"
-    key    = "app-builder-frontend"
-    region = "us-west-1"
-  }
 }
