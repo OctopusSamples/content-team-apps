2d97e50d3f59d4b7da9c755d2d601fb0b60e3ad9
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-10 07:18:59 +1000

    Missed a character

--- js/app-generator/terraform/backend.tf
+++ js/app-generator/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "app-builder-c81b45ae-50de-466c-8500-3845fd8b80c"
+    bucket = "app-builder-c81b45ae-50de-466c-8500-3845fd8b80c4"
     key    = "app-builder-frontend"
     region = "us-west-1"
   }
