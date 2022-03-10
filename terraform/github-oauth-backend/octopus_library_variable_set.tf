5b74d61e8a0d822815e0537bf5d176bdaaf7b04b
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-11 09:24:54 +1000

    Trying to find an empty value that breaks deployment

--- terraform/github-oauth-backend/octopus_library_variable_set.tf
+++ terraform/github-oauth-backend/octopus_library_variable_set.tf
@@ -7,6 +7,7 @@ output "library_variable_set_id" {
   value = octopusdeploy_library_variable_set.library_variable_set.id
 }
 
+
 resource "octopusdeploy_variable" "aws_development_account" {
   name = "AWS.Account"
   type = "AmazonWebServicesAccount"
@@ -46,5 +47,41 @@ resource "octopusdeploy_variable" "aws_s3_bucket" {
   description = "The name of the bucket hosting the App Builder frontend web app."
   is_sensitive = false
   owner_id = octopusdeploy_library_variable_set.library_variable_set.id
-  value = "AppBuilderFrontend"
+  value = "AppBuilderOAuthProxy"
+}
+
+resource "octopusdeploy_variable" "aws_cloudformation_frontend" {
+  name = "CloudFormation.Frontend"
+  type = "String"
+  description = "The name of the stack hosting the frontend API Gateway resources."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "AppBuilderFrontendApiGateway"
+}
+
+resource "octopusdeploy_variable" "aws_s3_directory" {
+  name = "S3.Directory"
+  type = "String"
+  description = "The S3 'directory' that holds the frontend web app files for a given deployment. This directory is based on the package version."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "#{Octopus.Action[Upload Frontend].Package[].PackageId}.#{Octopus.Action[Upload Frontend].Package[].PackageVersion}"
+}
+
+resource "octopusdeploy_variable" "cloudformation_apigateway" {
+  name = "CloudFormationName.ApiGateway"
+  type = "String"
+  description = "The Cloudformation stack that created the app builder API gateway."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "AppBuilderApiGateway"
+}
+
+resource "octopusdeploy_variable" "cloudformation_apigateway_stage" {
+  name = "CloudFormationName.ApiGatewayStage"
+  type = "String"
+  description = "The Cloudformation stack that created the app builder API gateway stage."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "AppBuilderApiGatewayStage"
 }
