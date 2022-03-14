daef1b8e8f976f67e5b34bfe7d924c31b623fd1b
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-15 08:48:04 +1000

    Added a lambda entry point

--- js/octopus-template-generator/src/application/lambda/generate-template.lambda.ts
+++ js/octopus-template-generator/src/application/lambda/generate-template.lambda.ts
@@ -1,9 +1,37 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import { deserialise } from 'kitsu-core'
+import {TemplateGenerator} from "../../domain/hanlders/templateGenerator";
+import * as fs from "fs";
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const queries = JSON.stringify(event.queryStringParameters);
+const PATH = "/api/generatetemplate";
+
+export async function lambdaHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+
+    if (event.path === PATH &&) {
+        return generateZip(event);
+    }
+
+    return {
+        statusCode: 404,
+        body: "Resource not found"
+    }
+}
+
+async function generateZip(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    // Convert the JSONAPI body to a plain object
+    const body = deserialise(event.body);
+
+    const templateZip = await new TemplateGenerator().generateTemplate(
+        body.data.generator,
+        body.data.options);
+
+    const data = fs.readFileSync(templateZip).toString('base64');
+
     return {
         statusCode: 200,
-        body: `Queries: ${queries}`
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        headers: {"Content-Type": "application/zip"},
+        body: data,
+        isBase64Encoded: true
     }
 }
