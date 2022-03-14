2f847e3c2048f5ad67385ba2babe3697ffde8003
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-14 13:21:48 +1000

    Started work on the service account creator project

---
+++ terraform/octopus-service-account-creator/octopus_library_variable_set.tf
@@ -0,0 +1,135 @@
+resource "octopusdeploy_library_variable_set" "library_variable_set" {
+  name = "Octopus Service Account Creator"
+  description = "Variables used when deploying the ctopus Service Account Creator"
+}
+
+output "library_variable_set_id" {
+  value = octopusdeploy_library_variable_set.library_variable_set.id
+}
+
+
+resource "octopusdeploy_variable" "aws_development_account" {
+  name = "AWS.Account"
+  type = "AmazonWebServicesAccount"
+  description = "The AWS account used to deploy the application. Don't edit these variables directly - update the Terraform files in [GitHub](https://github.com/OctopusSamples/content-team-apps/terraform) instead."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = var.octopus_development_aws_account_id
+  scope {
+    environments = [var.octopus_development_environment_id, var.octopus_development_security_environment_id]
+  }
+}
+
+resource "octopusdeploy_variable" "aws_production_account" {
+  name = "AWS.Account"
+  type = "AmazonWebServicesAccount"
+  description = "The AWS account used to deploy the application."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = var.octopus_production_aws_account_id
+  scope {
+    environments = [var.octopus_production_environment_id, var.octopus_production_security_environment_id]
+  }
+}
+
+resource "octopusdeploy_variable" "aws_region" {
+  name = "AWS.Region"
+  type = "String"
+  description = "The AWS region."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = var.aws_region
+}
+
+resource "octopusdeploy_variable" "aws_s3_bucket" {
+  name = "CloudFormation.S3Bucket"
+  type = "String"
+  description = "The name of the stack creating the App Builder GitHub OAuth proxy S3 bucket."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "OctopusServiceAccountCreator"
+}
+
+resource "octopusdeploy_variable" "aws_cloudformation_code" {
+  name = "CloudFormation.OctopusServiceAccountCreator"
+  type = "String"
+  description = "The name of the stack hosting the lambda that converts a code to a token."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "OctopusServiceAccountCreator"
+}
+
+resource "octopusdeploy_variable" "cloudformation_apigateway" {
+  name = "CloudFormationName.ApiGateway"
+  type = "String"
+  description = "The Cloudformation stack that created the app builder API gateway."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "AppBuilderApiGateway"
+}
+
+resource "octopusdeploy_variable" "cloudformation_apigateway_stage" {
+  name = "CloudFormationName.ApiGatewayStage"
+  type = "String"
+  description = "The Cloudformation stack that created the app builder API gateway stage."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "AppBuilderApiGatewayStage"
+}
+
+resource "octopusdeploy_variable" "cloudformation_encryption_key_production" {
+  name = "Client.EncryptionKey"
+  type = "Sensitive"
+  description = "The key used to encrypt the GitHub Oauth token sent back to the client in a cookie."
+  is_sensitive = true
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = var.github_proxy_encryption_key_production
+  scope {
+    environments = [var.octopus_production_environment_id, var.octopus_production_security_environment_id]
+  }
+}
+
+resource "octopusdeploy_variable" "cloudformation_encryption_key_development" {
+  name = "Client.EncryptionKey"
+  type = "Sensitive"
+  description = "The key used to encrypt the GitHub Oauth token sent back to the client in a cookie."
+  is_sensitive = true
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = var.github_proxy_encryption_key_development
+  scope {
+    environments = [var.octopus_development_security_environment_id, var.octopus_development_environment_id]
+  }
+}
+
+resource "octopusdeploy_variable" "cloudformation_encryption_salt_production" {
+  name = "Client.EncryptionSalt"
+  type = "String"
+  description = "The salt used to when encrypting the GitHub Oauth token sent back to the client in a cookie."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "bMjahk2IHvEVz5XgIaO82SfaHjwGMZQ9"
+  scope {
+    environments = [var.octopus_production_environment_id, var.octopus_production_security_environment_id]
+  }
+}
+
+resource "octopusdeploy_variable" "cloudformation_encryption_salt_development" {
+  name = "Client.EncryptionSalt"
+  type = "String"
+  description = "The salt used to when encrypting the GitHub Oauth token sent back to the client in a cookie."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "95FrLIDR5jFroc3MTD5QBd1EsnYKUh1e"
+  scope {
+    environments = [var.octopus_development_security_environment_id, var.octopus_development_environment_id]
+  }
+}
+
+resource "octopusdeploy_variable" "cloudformation_lambda_login" {
+  name = "Lambda.Name"
+  type = "String"
+  description = "The name of the Lambda that redirects users to the GitHub OAuth login."
+  is_sensitive = false
+  owner_id = octopusdeploy_library_variable_set.library_variable_set.id
+  value = "OctopusCreateAccountService"
+}
