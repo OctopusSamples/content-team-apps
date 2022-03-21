164e2640e9a5ba016499b8e37c8ef613a9643f1e
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-22 06:01:23 +1000

    Added more tests

--- java/azure-servicebus-proxy/src/test/java/com/octopus/loginmessage/infrastructure/octofront/impl/CommercialServiceBusImplTest.java
+++ java/azure-servicebus-proxy/src/test/java/com/octopus/loginmessage/infrastructure/octofront/impl/CommercialServiceBusImplTest.java
@@ -9,19 +9,30 @@ import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.octopus.loginmessage.CommercialAzureServiceBusTestProfile;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import java.time.ZonedDateTime;
+import java.util.List;
 import javax.inject.Inject;
 import org.apache.maven.shared.utils.StringUtils;
+import org.joda.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 /**
- * This test ensures the message generated for the service bus has the required properties as documented
- * in https://github.com/OctopusDeploy/Architecture/blob/main/OctopusHQ/Strategy.md.
+ * This test ensures the message generated for the service bus has the required properties as
+ * documented in https://github.com/OctopusDeploy/Architecture/blob/main/OctopusHQ/Strategy.md.
  */
 @QuarkusTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestProfile(CommercialAzureServiceBusTestProfile.class)
 public class CommercialServiceBusImplTest {
+
+  private static List<String> VALID_PROPERTIES = List.of(
+      "context",
+      "source",
+      "type",
+      "occurredTimeUtc",
+      "activityId",
+      "specVersion");
 
   @Inject
   CommercialServiceBusImpl commercialServiceBus;
@@ -29,19 +40,27 @@ public class CommercialServiceBusImplTest {
   @Test
   public void testGenerateMessage() {
     final ServiceBusMessage message = commercialServiceBus.generateMessage("trace", "body");
-    assertTrue(StringUtils.isNotBlank(message.getApplicationProperties().get("context").toString()));
-    assertTrue(StringUtils.isNotBlank(message.getApplicationProperties().get("source").toString()));
-    assertTrue(StringUtils.isNotBlank(message.getApplicationProperties().get("type").toString()));
-    assertTrue(StringUtils.isNotBlank(message.getApplicationProperties().get("occurredTimeUtc").toString()));
+
+    // All properties should be expected
+    message.getApplicationProperties().keySet().forEach(k -> assertTrue(
+        VALID_PROPERTIES.contains(k)));
+    // All properties should be set
+    message.getApplicationProperties().keySet().forEach(k -> assertTrue(
+        StringUtils.isNotBlank(message.getApplicationProperties().get(k).toString())));
+    // This field should be a ISO-8601 datetime
+    assertDoesNotThrow(() -> ZonedDateTime.parse(message.getApplicationProperties().get("occurredTimeUtc").toString()));
+    // These fields have known values
     assertEquals("body", message.getBody().toString());
     assertEquals("trace", message.getApplicationProperties().get("activityId").toString());
+    assertEquals("marketing", message.getApplicationProperties().get("context").toString());
     assertEquals("1", message.getApplicationProperties().get("specVersion").toString());
   }
 
   @Test
   public void testInvalidArgs() {
     assertThrows(NullPointerException.class, () -> commercialServiceBus.sendUserDetails("", null));
-    assertThrows(IllegalArgumentException.class, () -> commercialServiceBus.sendUserDetails("", ""));
+    assertThrows(IllegalArgumentException.class,
+        () -> commercialServiceBus.sendUserDetails("", ""));
   }
 
   @Test
