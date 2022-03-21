0daad41b3bc182b7d6ff811d27ac45abbadb575d
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-21 16:14:02 +1000

    Fixed checkstyle issues

--- java/azure-servicebus-proxy/src/main/java/com/octopus/loginmessage/domain/servicebus/impl/CommercialAzureServiceBus.java
+++ java/azure-servicebus-proxy/src/main/java/com/octopus/loginmessage/domain/servicebus/impl/CommercialAzureServiceBus.java
@@ -21,7 +21,10 @@ public class CommercialAzureServiceBus implements AzureServiceBus {
   ServiceBusConfig serviceBusConfig;
 
   @Override
-  public Optional<TokenCredential> getCredentials() {if (serviceBusConfig.appId().isEmpty() || serviceBusConfig.secret().isEmpty() || serviceBusConfig.tenant().isEmpty()) {
+  public Optional<TokenCredential> getCredentials() {
+    if (serviceBusConfig.appId().isEmpty()
+        || serviceBusConfig.secret().isEmpty()
+        || serviceBusConfig.tenant().isEmpty()) {
       return Optional.empty();
     }
 
