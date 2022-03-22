8b3bc778df22ed5fc70bfdd055fccf8e404f7098
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-22 16:24:35 +1000

    Removed the github docker feed

--- js/generator-github-shared-infrastructure/generators/app/templates/terraform/shared-infrastructure/feeds.tf
+++ js/generator-github-shared-infrastructure/generators/app/templates/terraform/shared-infrastructure/feeds.tf
@@ -6,14 +6,3 @@ resource "octopusdeploy_docker_container_registry" "dockerhub_feed" {
 output "octopus_dockerhub_feed_id" {
   value = octopusdeploy_docker_container_registry.dockerhub_feed.id
 }
-
-resource "octopusdeploy_docker_container_registry" "github_docker_feed" {
-  feed_uri = "https://gchr.io"
-  name     = "GitHub Docker"
-  password = var.github_feed_token
-  username = var.github_username
-}
-
-output "octopus_github_docker_feed_id" {
-  value = octopusdeploy_docker_container_registry.github_docker_feed.id
-}
