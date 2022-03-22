93236976b6f13c66fada36add8fb1e6d476b325d
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-22 18:00:08 +1000

    Make the new space open to everyone

--- js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
+++ js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
@@ -3,7 +3,7 @@ resource "octopusdeploy_space" "app_builder_space" {
   name                        = var.octopus_space
   is_default                  = false
   is_task_queue_stopped       = false
-  space_managers_team_members = [var.octopus_user_id]
+  space_managers_teams        = ["teams-everyone"]
 }
 
 output "octopus_space_id" {
