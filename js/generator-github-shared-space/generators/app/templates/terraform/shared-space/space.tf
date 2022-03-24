204e0d4ec6772dbcc32f288aabba5ba2e02cc5b9
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-24 11:24:16 +1000

    Exported the generated space name as an output variable.

--- js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
+++ js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
@@ -1,6 +1,6 @@
 resource "octopusdeploy_space" "app_builder_space" {
   description                 = "A space created by app builder. This resource is created and managed by the [Octopus Terraform provider](https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs). The Terraform files can be found in the [GitHub repo](https://github.com/${var.github_repo})."
-  name                        = substr("${var.octopus_space} ${split("/", var.github_repo)[0]}", 0, 20)
+  name                        = substr("${var.octopus_space} ${var.github_repo_owner}", 0, 20)
   is_default                  = false
   is_task_queue_stopped       = false
   space_managers_teams        = ["teams-everyone"]
@@ -8,4 +8,8 @@ resource "octopusdeploy_space" "app_builder_space" {
 
 output "octopus_space_id" {
   value = octopusdeploy_space.app_builder_space.id
+}
+
+output "octopus_space_name" {
+  value = octopusdeploy_space.app_builder_space.name
 }
