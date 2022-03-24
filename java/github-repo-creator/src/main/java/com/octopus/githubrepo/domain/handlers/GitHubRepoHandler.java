0c6f4b01ffc87206125ec504556d5790b1b95a0e
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-24 19:34:07 +1000

    Fixed the initial instructions.

--- java/github-repo-creator/src/main/java/com/octopus/githubrepo/domain/handlers/GitHubRepoHandler.java
+++ java/github-repo-creator/src/main/java/com/octopus/githubrepo/domain/handlers/GitHubRepoHandler.java
@@ -499,11 +499,12 @@ public class GitHubRepoHandler {
                         + "* `dotnet`: The sample DotNET application.\n\n"
                         + "If you have run the App Builder for a second time, the files are placed in the app-builder-update branch.\n"
                         + "The workflow files are configured to not run from this branch, meaning any changes you have made in the main branch will not be overwritten.\n"
-                        + "To \"replace\" the main branch with the app-builder-update branch, [run the following commands](https://stackoverflow.com/a/2862938/157605):\n"
+                        + "To replace the main branch with the app-builder-update branch, [run the following commands](https://stackoverflow.com/a/2862938/157605):\n"
                         + "1. `git checkout app-builder-update`\n"
                         + "2. `git merge -s ours main`\n"
                         + "3. `git checkout main`\n"
-                        + "4. `git checkout app-builder-update`\n")
+                        + "4. `git merge app-builder-update`\n\n"
+                        + "If you would rather see what has changed since you last ran the App Builder, create a regular pull request between the app-builder-update and main branches.")
 
                         .getBytes(StandardCharsets.UTF_8)))
                 .message("Adding the initial marker file")
