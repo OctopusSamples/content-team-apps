433abacd886b0d74d27fa570b64d428b5ad1c026
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-25 08:58:23 +1000

    Linked more variables

--- terraform/app-builder-frontend/vars.tf
+++ terraform/app-builder-frontend/vars.tf
@@ -61,3 +61,13 @@ variable "octopus_production_security_environment_id" {
   type = string
   nullable = false
 }
+
+variable "cognito_library_variable_set_id" {
+  type = string
+  nullable = false
+}
+
+variable "content_team_library_variable_set_id" {
+  type = string
+  nullable = false
+}
