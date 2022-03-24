92adb5db2c26e2adf2691800f62f9e49dade0c30
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-24 10:50:44 +1000

    args were in wrong order

--- js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
+++ js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
@@ -1,6 +1,6 @@
 resource "octopusdeploy_space" "app_builder_space" {
   description                 = "A space created by app builder. This resource is created and managed by the [Octopus Terraform provider](https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs). The Terraform files can be found in the [GitHub repo](https://github.com/${var.github_repo})."
-  name                        = substr("${var.octopus_space} ${split(var.github_repo, "/")[0]}", 0, 20)
+  name                        = substr("${var.octopus_space} ${split("/", var.github_repo)[0]}", 0, 20)
   is_default                  = false
   is_task_queue_stopped       = false
   space_managers_teams        = ["teams-everyone"]
