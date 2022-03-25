02f7bf94221b09d9bc49a688bc843760003f26e6
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-25 14:55:26 +1000

    Make a note that the process creates a new space

--- js/app-generator/src/components/journey/Done.tsx
+++ js/app-generator/src/components/journey/Done.tsx
@@ -42,7 +42,7 @@ const Done: FC<JourneyProps> = (props): ReactElement => {
                             workflow to complete.
                         </p>
                         <p>
-                            Once the GitHub Actions workflow has completed, the space named <strong>{props.machine.state.context.targetPlatform + " " + props.machine.state.context.developmentFramework}</strong> and
+                            Once the GitHub Actions workflow has completed, the new space named <strong>{props.machine.state.context.targetPlatform + " " + props.machine.state.context.developmentFramework}</strong> and
                             ending with your GitHub account name (or as many characters of your GitHub account name as we could fit in the space
                             name) in your <a href={getOctopusServer()}>Octopus instance</a> will be populated with a
                             sample project and all other associated resources to complete a deployment.
