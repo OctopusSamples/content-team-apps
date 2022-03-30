8589418315da70bfd5650302e6f91db43233328e
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-31 08:00:03 +1000

    Fixed up object serialization

--- java/github-actions-builder-github-backend/src/main/java/com/octopus/githubactions/github/domain/entities/GitHubEmail.java
+++ java/github-actions-builder-github-backend/src/main/java/com/octopus/githubactions/github/domain/entities/GitHubEmail.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 /**
  * The data returned by the GitHub public email endpoint.
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Jacksonized
 public class GitHubEmail {
   private String email;
 }
