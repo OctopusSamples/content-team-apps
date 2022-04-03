0fd2a044b3c45cecbf73b3abe3ab3225a29ff7c5
Matthew Casperson <matthewcasperson@gmail.com>
2022-04-04 07:12:05 +1000

    Fixed up ecr name

--- js/generator-github-complete-eks-deployment/generators/app/index.js
+++ js/generator-github-complete-eks-deployment/generators/app/index.js
@@ -8,7 +8,10 @@ module.exports = class extends Generator {
         this.option("aws_region", {type: String});
         this.option("aws_state_bucket_region", {type: String});
         this.option("platform", {type: String});
-        this.option("framework", {type: String});
+        this.option("framework", {
+            type: String,
+            description: "An optional field that defines the framework or language i.e. Spring, DotNET, Express etc. If defined, the framework is included the registry and Docker image names."
+        });
         this.option("s3_bucket_suffix", {type: String, default: crypto.randomUUID()});
     }
 
@@ -39,6 +42,7 @@ module.exports = class extends Generator {
         const framework = this.options["framework"];
         const platform = this.options["platform"];
         const spaceName = platform + (framework ? " " + framework : "");
+        const repository = "octopus-microservice" + (framework ? "-" + framework : "");
 
         this.fs.copyTpl(
             this.templatePath('.github/workflows/eks-deployment.yaml'),
@@ -48,7 +52,8 @@ module.exports = class extends Generator {
                 s3_bucket_suffix: this.options["s3BucketSuffix"],
                 aws_region: this.options["awsRegion"],
                 framework,
-                platform
+                platform,
+                repository
             }
         );
     }
