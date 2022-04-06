9bb366791a1768e204040aac76315de4f7a489d9
Matthew Casperson <matthewcasperson@gmail.com>
2022-04-06 12:45:25 +1000

    Removed unused code

--- js/app-generator/src/components/journey/TargetSelection.tsx
+++ js/app-generator/src/components/journey/TargetSelection.tsx
@@ -5,21 +5,6 @@ import {JourneyProps} from "../../statemachine/appBuilder";
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 
-function LinearProgressWithLabel(props: {value: number}) {
-    return (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Box sx={{ width: '100%', mr: 1 }}>
-                <LinearProgress variant="determinate" {...props} />
-            </Box>
-            <Box sx={{ minWidth: 35 }}>
-                <Typography variant="body2" color="text.secondary">{`${Math.round(
-                    props.value,
-                )}%`}</Typography>
-            </Box>
-        </Box>
-    );
-}
-
 const TargetSelection: FC<JourneyProps> = (props): ReactElement => {
     const classes = journeyContainer();
 
