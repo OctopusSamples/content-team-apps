ac430d3f3ddda59e2124fb7048c44ccdc108f63b
Matthew Casperson <matthewcasperson@gmail.com>
2022-04-12 08:03:28 +1000

    Fixed dependencies for channels

--- terraform/app-builder-frontend/channels.tf
+++ terraform/app-builder-frontend/channels.tf
@@ -2,6 +2,7 @@ resource "octopusdeploy_channel" "feature_branch" {
   name        = "Feature Branches"
   project_id  = octopusdeploy_project.deploy_project.id
   description = "The channel through which feature branches are deployed"
+  depends_on = [octopusdeploy_project.deploy_project]
   rule {
     tag = ".+"
     action_package {
@@ -14,6 +15,7 @@ resource "octopusdeploy_channel" "mainline" {
   name        = "Mainline"
   project_id  = octopusdeploy_project.deploy_project.id
   description = "The channel through which mainline releases are deployed"
+  depends_on = [octopusdeploy_project.deploy_project]
   is_default  = true
   rule {
     tag = "^$"
--- terraform/github-proxy/channels.tf
+++ terraform/github-proxy/channels.tf
@@ -2,6 +2,7 @@ resource "octopusdeploy_channel" "feature_branch" {
   name        = "Feature Branches"
   project_id  = octopusdeploy_project.deploy_project.id
   description = "The channel through which feature branches are deployed"
+  depends_on = [octopusdeploy_project.deploy_project]
   rule {
     tag = ".+"
     action_package {
@@ -15,6 +16,7 @@ resource "octopusdeploy_channel" "mainline" {
   project_id  = octopusdeploy_project.deploy_project.id
   description = "The channel through which mainline releases are deployed"
   is_default  = true
+  depends_on = [octopusdeploy_project.deploy_project]
   rule {
     tag = "^$"
     action_package {
