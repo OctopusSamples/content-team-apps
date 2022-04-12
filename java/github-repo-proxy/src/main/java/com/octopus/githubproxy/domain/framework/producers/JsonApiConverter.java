1cd5ccc48c79e6f1be284f3c3650d51e4b11b795
Matthew Casperson <matthewcasperson@gmail.com>
2022-04-12 14:30:03 +1000

    Fixed jsonapi serialization

--- java/github-repo-proxy/src/main/java/com/octopus/githubproxy/domain/framework/producers/JsonApiConverter.java
+++ java/github-repo-proxy/src/main/java/com/octopus/githubproxy/domain/framework/producers/JsonApiConverter.java
@@ -17,7 +17,7 @@ public class JsonApiConverter {
    */
   @Produces
   public ResourceConverter buildResourceConverter() {
-    final ResourceConverter resourceConverter = new ResourceConverter(GitHubRepo.class, Health.class, GitHubRepoMeta.class);
+    final ResourceConverter resourceConverter = new ResourceConverter(GitHubRepo.class, Health.class);
     resourceConverter.disableDeserializationOption(DeserializationFeature.REQUIRE_RESOURCE_ID);
     resourceConverter.enableDeserializationOption(DeserializationFeature.ALLOW_UNKNOWN_INCLUSIONS);
     return resourceConverter;
