9a984bbcf1a695e302cad082f6c366742abc163c
Matthew Casperson <matthewcasperson@gmail.com>
2022-04-12 13:17:16 +1000

    Added security channel to azure service bus proxy

--- terraform/azure-servicebus-proxy-jenkins/channels.tf
+++ terraform/azure-servicebus-proxy-jenkins/channels.tf
@@ -24,3 +24,18 @@ resource "octopusdeploy_channel" "mainline" {
     }
   }
 }
+
+resource "octopusdeploy_channel" "security" {
+  name        = "Security Testing"
+  project_id  = octopusdeploy_project.deploy_project.id
+  description = "The channel used to test security"
+  depends_on = [octopusdeploy_project.deploy_project]
+  lifecycle_id = var.octopus_security_lifecycle_id
+  is_default  = false
+  rule {
+    tag = "^$"
+    action_package {
+      deployment_action = "Upload Frontend"
+    }
+  }
+}
