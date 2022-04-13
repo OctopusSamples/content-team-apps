258c37d7ba6bfbde7044a07aeb918d3c4a968b12
Matthew Casperson <matthewcasperson@gmail.com>
2022-04-13 11:34:53 +1000

    Restored another project

--- terraform/github-actions-workflows/project_groups.tf
+++ terraform/github-actions-workflows/project_groups.tf
@@ -2,7 +2,3 @@ resource "octopusdeploy_project_group" "project_group" {
   description  = "The GitHub Actions Workflows Generator."
   name         = "GitHub Actions Workflows Generator"
 }
-
-output "appbuilder_frontend_project_group_id" {
-  value = octopusdeploy_project_group.project_group.id
-}
