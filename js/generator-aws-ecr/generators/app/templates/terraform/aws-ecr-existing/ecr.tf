949d294a1eeeb67e5bf6c2949f5ea30d07e104b7
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-10 10:26:33 +1000

    Fixed an issue with a missing output variable.

--- js/generator-aws-ecr/generators/app/templates/terraform/aws-ecr-existing/ecr.tf
+++ js/generator-aws-ecr/generators/app/templates/terraform/aws-ecr-existing/ecr.tf
@@ -1,3 +1,4 @@
+# Lookup the existing ECR instance and use that as the basis for the Octopus feed
 data "aws_ecr_repository" "aws_ecr_repository" {
   name = "${var.aws_ecr_repository_name}-${lower(var.github_repo_owner)}-${lower(var.platform)}"
 }
