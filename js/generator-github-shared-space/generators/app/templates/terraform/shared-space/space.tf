bde8b13511cc7f28e33b214818bfd185a3ecb820
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-11 07:21:59 +1000

    Attempt to generate a unique space name

--- js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
+++ js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
@@ -1,6 +1,18 @@
+data "octopusdeploy_spaces" "app_builder_space" {
+  partial_name = substr("${var.octopus_space} ${lower(var.github_repo_owner)}", 0, 18)
+  skip         = 0
+  take         = 100
+}
+
 resource "octopusdeploy_space" "app_builder_space" {
   description                 = "A space created by app builder. This resource is created and managed by the [Octopus Terraform provider](https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs). The Terraform files can be found in the [GitHub repo](https://github.com/${var.github_repo})."
-  name                        = substr("${var.octopus_space} ${lower(var.github_repo_owner)}", 0, 20)
+  # Attempt to generate a uniquely named space.
+  # We can get into a situation where the provider is rerun with a fresh backend, and loses the ID of a previously
+  # created space. For example, the GitHub repo may be deleted and recreated, resulting in a fresh backend.
+  # In this case we want to create a new, unique space. This is achieved by scanning the Octopus instance
+  # for any existing spaces with the same prefix, getting the count, adding 1, and using that as the suffix.
+  # This is obviously not completely accurate, as spaces may have been renamed, but should catch most edge cases.
+  name                        = "${substr("${var.octopus_space} ${lower(var.github_repo_owner)}", 0, 18)} ${data.octopusdeploy_spaces.app_builder_space.count + 1}"
   is_default                  = false
   is_task_queue_stopped       = false
   space_managers_teams        = ["teams-everyone"]
