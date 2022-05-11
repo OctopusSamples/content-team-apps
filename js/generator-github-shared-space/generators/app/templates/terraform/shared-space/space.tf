2a688b1cce6c4dff2bee6f6fc653f6815c15552b
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-11 10:29:21 +1000

    Account for an existing space

--- js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
+++ js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
@@ -1,6 +1,6 @@
 resource "octopusdeploy_space" "app_builder_space" {
   description           = "A space created by app builder. This resource is created and managed by the [Octopus Terraform provider](https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs). The Terraform files can be found in the [GitHub repo](https://github.com/${var.github_repo})."
-  name                  = substr("${var.octopus_space} ${lower(var.github_repo_owner)}", 0, 20)
+  name                  = var.octopus_space
   is_default            = false
   is_task_queue_stopped = false
   space_managers_teams  = ["teams-everyone"]
