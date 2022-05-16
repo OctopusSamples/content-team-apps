b0e567b2b266148df07a857beac4ad5df2fa908a
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-17 07:23:12 +1000

    replace render with createRoot

--- js/generator-octopus-javascript-frontend/generators/app/templates/js/src/index.tsx
+++ js/generator-octopus-javascript-frontend/generators/app/templates/js/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {loadConfig} from "./dynamicConfig";
+import {createRoot} from "react-dom/client";
 
 loadConfig().then((settings) => {
-    ReactDOM.render(
+    const container = document.getElementById('root')!;
+    const root = createRoot(container);
+    root.render(
         <React.StrictMode>
             <App {...settings}/>
         </React.StrictMode>,
-        document.getElementById('root')
     );
 })
 
