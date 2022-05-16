1eb67e1027d518933bb6b7079e8b7db15493542e
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-17 07:35:56 +1000

    Moved some files to potentially support more js apps in future

---
+++ js/generator-octopus-javascript-frontend/generators/app/templates/index.js
@@ -0,0 +1,35 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+
+        this.option("s3_bucket_suffix", {type: String});
+        this.option("aws_state_bucket_region", {type: String});
+        this.option("aws_region", {type: String});
+    }
+
+    writing() {
+        this.fs.copyTpl(
+            this.templatePath('js/**/*'),
+            this.destinationPath('js'),
+            {
+                s3_bucket_suffix: this.options["s3_bucket_suffix"],
+                aws_state_bucket_region: this.options["aws_state_bucket_region"],
+                aws_region: this.options["aws_region"]
+            },
+            null,
+            {globOptions: {ignore: ["**/build", "**/*.iml", "**/.idea"]}}
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('github/java-frontend/action.yaml'),
+            this.destinationPath('github/js-frontend/action.yaml'),
+            {
+                s3_bucket_suffix: this.options["s3_bucket_suffix"],
+                aws_state_bucket_region: this.options["aws_state_bucket_region"],
+                aws_region: this.options["aws_region"]
+            }
+        )
+    }
+};
--- js/generator-octopus-javascript-frontend/generators/app/templates/js/README.md
+++
@@ -1,2 +0,0 @@
-https://www.welcomedeveloper.com/react-typescript-material-design
-https://github.com/danilocastronz/weldev-project-react-mui-ts-bp
