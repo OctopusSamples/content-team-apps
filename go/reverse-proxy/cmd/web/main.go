f6f38e6b325fc7529cbec75d4f6995228bf8c19e
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-20 14:19:25 +1000

    Started work on a http interface for the proxy

---
+++ go/reverse-proxy/cmd/web/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"github.com/OctopusSamples/content-team-apps/go/reverse-proxy/internal/pkg/lambdahandler"
+	"github.com/OctopusSamples/content-team-apps/go/reverse-proxy/internal/pkg/utils"
+	"github.com/aws/aws-lambda-go/events"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	port := utils.GetEnv("PORT", "8080")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		newStr := buf.String()
+
+		request := events.APIGatewayProxyRequest{
+			MultiValueHeaders:               r.Header,
+			Path:                            r.RequestURI,
+			Body:                            newStr,
+			HTTPMethod:                      r.Method,
+			IsBase64Encoded:                 false,
+			MultiValueQueryStringParameters: r.URL.Query(),
+		}
+		response, _ := lambdahandler.HandleRequest(nil, request)
+
+		w.WriteHeader(response.StatusCode)
+
+		for s, s2 := range response.Headers {
+			w.Header().Add(s, s2)
+		}
+
+		w.Write([]byte(response.Body))
+	})
+
+	log.Println("Listening on port " + port)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
