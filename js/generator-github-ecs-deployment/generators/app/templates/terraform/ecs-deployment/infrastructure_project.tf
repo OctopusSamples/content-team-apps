b5f0578272a2c201e2fcd31399ceb019ed99ba2b
Matthew Casperson <matthewcasperson@gmail.com>
2022-05-30 09:38:46 +1000

    Renamed shared load balancer to be unique between users

--- js/generator-github-ecs-deployment/generators/app/templates/terraform/ecs-deployment/infrastructure_project.tf
+++ js/generator-github-ecs-deployment/generators/app/templates/terraform/ecs-deployment/infrastructure_project.tf
@@ -252,7 +252,7 @@ resource "octopusdeploy_deployment_process" "deploy_cluster" {
             ApplicationLoadBalancer:
               Type: "AWS::ElasticLoadBalancingV2::LoadBalancer"
               Properties:
-                Name: "ECS-LB-Shared"
+                Name: "ECS-LB-Shared-${lower(var.github_repo_owner)}"
                 Scheme: "internet-facing"
                 Type: "application"
                 Subnets:
