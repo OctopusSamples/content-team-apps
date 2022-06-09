ec9e4b7255943c4fac6886ab1d6ad662f11d0e42
Matthew Casperson <matthewcasperson@gmail.com>
2022-06-09 14:16:19 +1000

    Fixed links

--- js/generator-github-lambda-deployment/generators/app/templates/terraform/lambda-deployment/frontend_deployment_project.tf
+++ js/generator-github-lambda-deployment/generators/app/templates/terraform/lambda-deployment/frontend_deployment_project.tf
@@ -759,7 +759,7 @@ resource "octopusdeploy_deployment_process" "deploy_frontend" {
           set_octopusvariable "DNSName" $${DNS_NAME}
           echo "DNS Name: $DNS_NAME"
 
-          write_highlight "Open [$${STAGE_URL}index.html]($${STAGE_URL}#index.html) to view the frontend web app."
+          write_highlight "Open [$${STAGE_URL}index.html]($${STAGE_URL}index.html) to view the frontend web app."
         EOT
         "Octopus.Action.Script.ScriptSource" : "Inline"
         "Octopus.Action.Script.Syntax" : "Bash"
