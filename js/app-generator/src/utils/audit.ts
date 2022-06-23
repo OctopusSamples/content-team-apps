da6794c366cc255d1a49c2cc80972067638e91d5
Matthew Casperson <matthewcasperson@gmail.com>
2022-06-23 10:16:26 +1000

    Make audits async

--- js/app-generator/src/utils/audit.ts
+++ js/app-generator/src/utils/audit.ts
@@ -3,13 +3,22 @@ import {RuntimeSettings} from "../config/runtimeConfig";
 
 export function auditPageVisit(page: string, settings: RuntimeSettings, partition?: string | null) {
     postJsonApi(JSON.stringify({
-        data: {
-            type: "audits",
-            attributes: {
-                subject: "GitHubRepoFrontend",
-                action: "VisitedPage",
-                object: page
+            data: {
+                type: "audits",
+                attributes: {
+                    subject: "GitHubRepoFrontend",
+                    action: "VisitedPage",
+                    object: page
+                }
             }
-        }
-    }), settings.auditEndpoint, settings, partition);
+        }),
+        settings.auditEndpoint,
+        settings,
+        partition,
+        true,
+        () => {
+            const headers = new Headers();
+            headers.set('Invocation-Type', 'Event');
+            return headers;
+        });
 }
