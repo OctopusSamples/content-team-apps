3cfa529e6182685e63d3936c745f1f936c79d812
Matthew Casperson <matthewcasperson@gmail.com>
2022-06-27 16:00:35 +1000

    Added some comments

--- js/octopus-template-generator/src/domain/yeoman/adapter.ts
+++ js/octopus-template-generator/src/domain/yeoman/adapter.ts
@@ -1,9 +1,12 @@
+/**
+ * A no-op adapter that fails if any questions are asked, as there is no way to respond from a REST API.
+ */
 export class NonInteractiveAdapter {
     prompt(questions: Object|Object[], answers: never, cb: never) {
         if (Array.isArray(questions)) {
-            questions.forEach(q => console.log("Was promted for " + q));
+            questions.forEach(q => console.log("Was prompted for " + q));
         } else {
-            console.log("Was promted for " + questions)
+            console.log("Was prompted for " + questions)
         }
         throw new Error("Was prompted for questions");
     }
