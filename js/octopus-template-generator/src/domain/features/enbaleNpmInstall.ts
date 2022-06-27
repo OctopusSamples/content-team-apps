ddc9282c0e5a1025c5f0bfb4824d8758c3584f73
Matthew Casperson <matthewcasperson@gmail.com>
2022-06-27 15:28:45 +1000

    Added more comments

--- js/octopus-template-generator/src/domain/features/enbaleNpmInstall.ts
+++ js/octopus-template-generator/src/domain/features/enbaleNpmInstall.ts
@@ -1,3 +1,6 @@
+/**
+ * Defines whether we allow missing generators to be downloaded when they are requested.
+ */
 export function enableNpmInstall() : boolean {
     return process.env.ENABLE_NPM_INSTALL?.toLowerCase() === "true";
 }
