78f0c37eacf5218480f616ddc5354a5f22cc2c58
Matthew Casperson <matthewcasperson@gmail.com>
2022-07-01 16:25:52 +1000

    Added comments

--- js/yeoman-input-inspector/adapter.js
+++ js/yeoman-input-inspector/adapter.js
@@ -1,7 +1,9 @@
 import TerminalAdapter from "yeoman-environment/adapter";
 
 /**
- * An adapter that reads the questions and then exits.
+ * An adapter that reads the questions and then exits. This allows developers to
+ * see the questions that they must provide answers to when generating templates
+ * non-interactively, without having to actually generate any files.
  */
 export default class LoggingAdapter extends TerminalAdapter {
     constructor() {
