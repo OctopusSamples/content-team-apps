eef17d151db5c53cfecbcf57a4f42c16486e1c22
Matthew Casperson <matthewcasperson@gmail.com>
2022-07-12 07:12:56 +1000

    The generateTemplateSync now waits for the lock file to be cleared and returns, while generateTemplate returns if a lock file is found.

--- js/octopus-template-generator/src/domain/utils/fileLockProcess.ts
+++ js/octopus-template-generator/src/domain/utils/fileLockProcess.ts
@@ -1,8 +1,28 @@
 const lockFile = require('lockfile');
 
-export default function lockFileAndContinue(lockFilePath: string, cb: () => unknown) {
+/**
+ * A reasonable amount of time to wait for any generator to complete.
+ */
+const WAIT_TIME = 60000;
+/**
+ * We don't need to be too aggressive with polling.
+ */
+const POLL_PERIOD = 1000;
+/**
+ * Stale locks can occur if you kill the server with after a lock file is created.
+ * Assume locks are stale after some multiple of the WAIT_TIME.
+ */
+const STALE_PERIOD = WAIT_TIME * 3;
+
+export default function lockFileAndContinue(lockFilePath: string, wait: boolean, cb: () => unknown) {
+    /*
+     If we are waiting for the lock, define the wait and pollPeriod options.
+     We always set the stale option to deal with old locks.
+     */
+    const options = {...(wait ? {wait: WAIT_TIME, pollPeriod: POLL_PERIOD} : {}), stale: STALE_PERIOD};
+
     return new Promise((resolve, reject) => {
-        lockFile.lock(lockFilePath, (err: never) => {
+        lockFile.lock(lockFilePath, options, (err: never) => {
             if (err) return reject(err);
             return resolve(cb());
         })
