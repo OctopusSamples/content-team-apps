ba337a45c9859da553284f100e8e4b254e7acbdf
Matthew Casperson <matthewcasperson@gmail.com>
2022-07-15 13:58:31 +1000

    Bucket names now include the repo and owner

--- js/generator-octopus-project/generators/octopusbaseaws/templates/terraform/octopus/backend_aws.tf
+++ js/generator-octopus-project/generators/octopusbaseaws/templates/terraform/octopus/backend_aws.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "workflow-builder-${PROJECT_NAME}"
+    bucket = "workflow-builder-${GITHUB_OWNER}-${GITHUB_REPO}-${PROJECT_NAME}"
     key    = "appbuilder-ecr-deployment"
     region = "${TERRAFORM_BUCKET_REGION}"
   }
