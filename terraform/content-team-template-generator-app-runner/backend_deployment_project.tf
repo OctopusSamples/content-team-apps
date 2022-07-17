822c9814a0afa9c6d73a8eb5e89f098a94d01cf2
Matthew Casperson <matthewcasperson@gmail.com>
2022-07-18 08:38:24 +1000

    Move scripts to their own files

--- terraform/content-team-template-generator-app-runner/backend_deployment_project.tf
+++ terraform/content-team-template-generator-app-runner/backend_deployment_project.tf
@@ -269,33 +269,7 @@ resource "octopusdeploy_deployment_process" "deploy_backend" {
         "Octopus.Action.Aws.Region" : var.aws_region
         "Octopus.Action.AwsAccount.UseInstanceRole" : "False"
         "Octopus.Action.AwsAccount.Variable" : "AWS Account"
-        "Octopus.Action.Script.ScriptBody" : <<-EOT
-          echo "Downloading Docker images"
-
-          echo "##octopus[stdout-verbose]"
-
-          docker pull amazon/aws-cli 2>&1
-
-          # Alias the docker run commands
-          shopt -s expand_aliases
-          alias aws="docker run --rm -i -v $(pwd):/build -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
-
-          echo "##octopus[stdout-default]"
-
-          aws apprunner start-deployment --service-arn "#{Octopus.Action[Deploy App Runner Instance].Output.AwsOutputs[ServiceArn]}"
-
-          for i in {1..60}
-          do
-            STATUS=$(aws apprunner describe-service --service-arn "#{Octopus.Action[Deploy App Runner Instance].Output.AwsOutputs[ServiceArn]}" | jq -r '.Service.Status')
-            echo "App Runner status is $${STATUS}"
-            if [[ $${STATUS} == "RUNNING" ]]; then
-              break
-            fi
-
-            echo "Sleeping for 10 seconds"
-            sleep 10
-          done
-        EOT
+        "Octopus.Action.Script.ScriptBody" : file("start-deployment.sh")
         "Octopus.Action.Script.ScriptSource" : "Inline"
         "Octopus.Action.Script.Syntax" : "Bash"
         "OctopusUseBundledTooling" : "False"
