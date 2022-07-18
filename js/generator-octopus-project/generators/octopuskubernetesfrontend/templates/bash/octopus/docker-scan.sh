dd56ec842e663885cd12d58b4a4a2bee5fcde036
Matthew Casperson <matthewcasperson@gmail.com>
2022-07-18 12:43:41 +1000

    Fixing up the docker scan script

--- js/generator-octopus-project/generators/octopuskubernetesfrontend/templates/bash/octopus/docker-scan.sh
+++ js/generator-octopus-project/generators/octopuskubernetesfrontend/templates/bash/octopus/docker-scan.sh
@@ -1,1 +1,7 @@
-docker scan ${image}:#{Octopus.Action[Frontend WebApp].Package[${frontend}].PackageVersion}
+# Manual docker scan plugin install instructions
+# https://github.com/docker/scan-cli-plugin#on-linux
+mkdir -p ~/.docker/cli-plugins
+curl https://github.com/docker/scan-cli-plugin/releases/latest/download/docker-scan_linux_amd64 -L -s -S -o ~/.docker/cli-plugins/docker-scan
+chmod +x ~/.docker/cli-plugins/docker-scan
+
+docker scan --accept-license ${image}:#{Octopus.Action[Frontend WebApp].Package[${frontend}].PackageVersion}
