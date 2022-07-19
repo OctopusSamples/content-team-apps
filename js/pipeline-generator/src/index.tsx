b8dd3811b85247160a2b3af6438dc600a4d8e067
Matthew Casperson <matthewcasperson@gmail.com>
2022-07-20 06:28:57 +1000

    Added new google tag manager

--- js/pipeline-generator/src/index.tsx
+++ js/pipeline-generator/src/index.tsx
@@ -71,20 +71,28 @@ reportWebVitals();
 function setupGoogleAnalytics(tag: string) {
     if (!tag) return;
 
-    addScript("https://www.googletagmanager.com/gtag/js?id=" + tag)
-    window.dataLayer = window.dataLayer || [];
+    addScript("(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
+        "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
+        "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
+        "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
+        "})(window,document,'script','dataLayer','" + tag + "');\n")
+    addNoScript("https://www.googletagmanager.com/ns.html?id=" + tag);
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
+}
 
-    gtag('js', new Date());
-    gtag('config', tag);
+function addNoScript(iframeSrc) {
+    const s = document.createElement('noscript');
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute("src", iframeSrc);
+    iframe.setAttribute("height", "0");
+    iframe.setAttribute("width", "0");
+    iframe.setAttribute("style", "display:none;visibility:hidden");
+    s.appendChild(iframe);
+    document.body.prepend(s);
 }
 
 function addScript(src) {
     const s = document.createElement('script');
-    s.setAttribute('src', src);
-    s.setAttribute('async', 'async')
-    document.body.appendChild(s);
+    s.textContent = src;
+    document.head.appendChild(s);
 }
