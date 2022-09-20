052fab73ec33f220f94e80572ec644a193137d1c
Matthew Casperson <matthewcasperson@gmail.com>
2022-09-21 07:59:39 +1000

    Fixed terraform indexes and bumped versions

--- js/generator-octopus-project/generators/octopusazureaccounts/templates/terraform/octopus/github_secrets.tf
+++ js/generator-octopus-project/generators/octopusazureaccounts/templates/terraform/octopus/github_secrets.tf
@@ -2,12 +2,12 @@ resource "github_actions_secret" "development_azure_account" {
   count           = var.development_azure_application_id != "" ? 1 : 0
   repository      = var.github_repo
   secret_name     = "OCTOPUS_AZURE_DEVELOPMENT_ACCOUNT_ID"
-  plaintext_value = octopusdeploy_azure_service_principal.development_azure_account.id
+  plaintext_value = octopusdeploy_azure_service_principal.development_azure_account[0].id
 }
 
 resource "github_actions_secret" "production_aws_access_key" {
   count           = var.production_azure_application_id != "" ? 1 : 0
   repository      = var.github_repo
   secret_name     = "OCTOPUS_AZURE_PRODUCTION_ACCOUNT_ID"
-  plaintext_value = octopusdeploy_azure_service_principal.production_azure_account.id
+  plaintext_value = octopusdeploy_azure_service_principal.production_azure_account[0].id
 }
