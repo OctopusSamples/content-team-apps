59aa9b4e3d5d47a31f5ebd379b056aecebf3d62e
Matthew Casperson <matthewcasperson@gmail.com>
2022-10-31 06:42:08 +1000

    yeoman generator now successfully generates files

--- js/generator-octopus-project/generators/googlemicroservicesdemo/index.js
+++ js/generator-octopus-project/generators/googlemicroservicesdemo/index.js
@@ -1,24 +1,116 @@
 const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
-    writing() {
-        this.fs.copyTpl(
-            this.templatePath('github/kubernetes-deployment/action.yaml'),
-            this.destinationPath('github/kubernetes-deployment/action.yaml'),
+    async prompting() {
+        this.answers = await this.prompt([
             {
-                s3_bucket_suffix: this.options["s3_bucket_suffix"],
-                aws_state_bucket_region: this.options["aws_state_bucket_region"],
-                aws_region: this.options["aws_region"]
+                type: "input",
+                name: "project_name",
+                message: "The Project Name",
+                default: "googlemicroservicesdemo"
+            },
+            {
+                type: "input",
+                name: "octopus_server",
+                message: "The hostname of your Octopus instance.",
+                default: "${{ secrets.OCTOPUS_SERVER }}"
+            },
+            {
+                type: "password",
+                name: "octopus_apikey",
+                message: "The Octopus API key.",
+                default: "${{ secrets.OCTOPUS_APIKEY }}"
+            },
+            {
+                type: "input",
+                name: "octopus_space_id",
+                message: "The Octopus Space",
+                default: "${{ secrets.OCTOPUS_SPACE_ID }}"
+            },
+            {
+                type: "input",
+                name: "octopus_lifecycle_id",
+                message: "The lifecycle to assign to the project",
+                default: "${{ secrets.OCTOPUS_APPLICATION_LIFECYCLE_ID }}"
+            },
+            {
+                type: "input",
+                name: "aws_access_key",
+                message: "The AWS access key.",
+                default: "${{ secrets.AWS_ACCESS_KEY_ID }}"
+            },
+            {
+                type: "password",
+                name: "aws_secret_key",
+                message: "The AWS secret key.",
+                default: "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+            },
+            {
+                type: "input",
+                name: "aws_region",
+                message: "The AWS region to create the state bucket in",
+                default: "us-west-1"
+            },
+            {
+                type: "input",
+                name: "octopus_development_app_environment_id",
+                message: "The development environment ID",
+                default: "${{ secrets.OCTOPUS_DEVELOPMENT_ENVIRONMENT_ID }}"
+            },
+            {
+                type: "input",
+                name: "octopus_development_security_environment_id",
+                message: "The development security environment ID",
+                default: "${{ secrets.OCTOPUS_DEVELOPMENT_SECURITY_ENVIRONMENT_ID }}"
+            },
+            {
+                type: "input",
+                name: "octopus_production_app_environment_id",
+                message: "The production environment ID",
+                default: "${{ secrets.OCTOPUS_PRODUCTION_ENVIRONMENT_ID }}"
+            },
+            {
+                type: "input",
+                name: "octopus_production_security_environment_id",
+                message: "The production security environment ID",
+                default: "${{ secrets.OCTOPUS_PRODUCTION_SECURITY_ENVIRONMENT_ID }}"
             }
+        ]);
+    }
+
+    writing() {
+        const options = {
+            project_name: this.answers["project_name"],
+            existing_project_group: this.answers["existing_project_group"],
+            aws_access_key: this.answers["aws_access_key"],
+            aws_secret_key: this.answers["aws_secret_key"],
+            aws_region: this.answers["aws_region"],
+            octopus_server: this.answers["octopus_server"],
+            octopus_apikey: this.answers["octopus_apikey"],
+            octopus_space_id: this.answers["octopus_space_id"],
+            octopus_development_app_environment_id: this.answers["octopus_development_app_environment_id"],
+            octopus_development_security_environment_id: this.answers["octopus_development_security_environment_id"],
+            octopus_production_app_environment_id: this.answers["octopus_production_app_environment_id"],
+            octopus_production_security_environment_id: this.answers["octopus_production_security_environment_id"],
+            octopus_dockerhub_feed_id: this.answers["octopus_dockerhub_feed_id"],
+            octopus_application_lifecycle_id: this.answers["octopus_application_lifecycle_id"],
+        };
+
+        this.fs.copyTpl(
+            this.templatePath('.github/workflows/octopus.yaml'),
+            this.destinationPath('.github/workflows/' + this.answers["project_name"] + '.yaml'),
+            options
         );
 
         this.fs.copyTpl(
-            this.templatePath('terraform/kubernetes-deployment/*.tf'),
-            this.destinationPath('terraform/kubernetes-deployment'),
-            {
-                s3_bucket_suffix: this.options["s3_bucket_suffix"],
-                aws_state_bucket_region: this.options["aws_state_bucket_region"],
-                aws_region: this.options["aws_region"]
-            },
+            this.templatePath('github/octopus/action.yaml'),
+            this.destinationPath('github/' + this.answers["project_name"] + '/action.yaml'),
+            options
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('terraform/octopus/*.tf'),
+            this.destinationPath('terraform/' + this.answers["project_name"]),
+            options,
             null,
             { globOptions: { dot: true } }
         );
