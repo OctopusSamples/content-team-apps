a671f12fd63643f8b00ed681c82183b4cbf0859f
Matthew Casperson <matthewcasperson@gmail.com>
2022-11-04 15:23:45 +1000

    Fixed default worker pool

--- js/generator-octopus-project/generators/googlemicroservicesdemo/templates/terraform/octopus/shared_locals.tf
+++ js/generator-octopus-project/generators/googlemicroservicesdemo/templates/terraform/octopus/shared_locals.tf
@@ -5,6 +5,6 @@ locals {
   feature_branch_namespace = "${var.namespace_prefix}onlineboutique-development"
   deployment_step          = "Deploy App"
   deployment_role          = "demo-k8s-cluster"
-  worker_pool_id           = data.octopusdeploy_worker_pools.vm_worker_pool.worker_pools[0].id
-  # worker_pool_id      = data.octopusdeploy_worker_pools.ubuntu_worker_pool.worker_pools[0].id
+  # worker_pool_id           = data.octopusdeploy_worker_pools.vm_worker_pool.worker_pools[0].id
+  worker_pool_id           = data.octopusdeploy_worker_pools.ubuntu_worker_pool.worker_pools[0].id
 }
