1483e2f1fbe8817ce86b86ce92cc7e18554a3221
Matthew Casperson <matthewcasperson@gmail.com>
2022-11-11 09:30:46 +1000

    Updated provider to fix space creation

--- js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
+++ js/generator-github-shared-space/generators/app/templates/terraform/shared-space/space.tf
@@ -1,9 +1,10 @@
 resource "octopusdeploy_space" "app_builder_space" {
-  description           = "A space created by Octopus Builder. This resource is created and managed by the [Octopus Terraform provider](https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs). The Terraform files can be found in the [GitHub repo](https://github.com/${var.github_repo})."
-  name                  = var.octopus_space
-  is_default            = false
-  is_task_queue_stopped = false
-  space_managers_teams  = ["teams-managers"]
+  description                 = "A space created by Octopus Builder. This resource is created and managed by the [Octopus Terraform provider](https://registry.terraform.io/providers/OctopusDeployLabs/octopusdeploy/latest/docs). The Terraform files can be found in the [GitHub repo](https://github.com/${var.github_repo})."
+  name                        = var.octopus_space
+  is_default                  = false
+  is_task_queue_stopped       = false
+  space_managers_teams        = ["teams-managers"]
+  space_managers_team_members = []
 }
 
 output "octopus_space_id" {
