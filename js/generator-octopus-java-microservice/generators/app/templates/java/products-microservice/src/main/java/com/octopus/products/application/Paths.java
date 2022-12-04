fb24720e0e21266f1d27405f1cce3a6a62429d09
Matthew Casperson <matthewcasperson@gmail.com>
2022-12-05 07:45:47 +1000

    Updated quarkus and added some more notes

--- js/generator-octopus-java-microservice/generators/app/templates/java/products-microservice/src/main/java/com/octopus/products/application/Paths.java
+++ js/generator-octopus-java-microservice/generators/app/templates/java/products-microservice/src/main/java/com/octopus/products/application/Paths.java
@@ -2,6 +2,19 @@ package com.octopus.products.application;
 
 /**
  * The paths used for tests. Update these to reflect the paths exposed by the application.
+ *
+ * The health check endpoints are inspired by the Kubernetes livez API endpoint (
+ * https://kubernetes.io/docs/reference/using-api/health-checks/#individual-health-checks) where
+ * each individual service can be queried for its health with a call to
+ * a URL like https://localhost:6443/livez/etcd.
+ *
+ * In this microservice architecture, we assume each path and HTTP method could be served by a
+ * unique microsrevice. This means each service is identified by the JSON API path (i.e. products
+ * or products/1) and the HTTP method (i.e. GET) to create an identifier /products/POST or
+ * /products/1/GET.
+ *
+ * This means a GET request to /health/products/POST will return the health of the microservice
+ * responsible for creating new products.
  */
 public final class Paths {
   public static final String API_ENDPOINT = "/api/products";
